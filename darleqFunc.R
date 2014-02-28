1760025fe725bbfdaec7aa16728176c1185a2a61
Tim <tim@ubuntu.(none)>
2014-02-28 01:08:23 +0000

    NAs ignored - tidying up code

--- darleqFunc.R
+++ darleqFunc.R
@@ -1,3 +1,5 @@
+### function requires #Data #Code #sampleID #siteID #Abundance #Taxa
+
 darleqFunc <- function(diatomTDI2){
   
   require(lubridate)
@@ -9,21 +11,11 @@ darleqFunc <- function(diatomTDI2){
   
   diatomTDI2$Alk <- as.numeric(diatomTDI2$Alk)
   diatomTDI2$Alk[diatomTDI2$Alk > 250] <- 250
-  diatomTDI2$TDI3[which(is.na(diatomTDI2$TDI3))] <- 0 # remove NAs for lapply function in next section
-  diatomTDI2$TDI4[which(is.na(diatomTDI2$TDI4))] <- 0
-  diatomTDI2$LTDI1[which(is.na(diatomTDI2$LTDI1))] <- 0
-  diatomTDI2$LTDI2[which(is.na(diatomTDI2$LTDI2))] <- 0
-  diatomTDI2$LTDI3[which(is.na(diatomTDI2$LTDI3))] <- 0
-  # diatomTDI2$LTDLR[which(is.na(diatomTDI2$LTDLR))] <- 0 # remove NAs for lapply function in next section
   diatomTDI2$TDI4 <-  as.numeric(diatomTDI2$TDI4)
-  diatomTDI2$TDI4[diatomTDI2$TDI4 == "NA"] <-  0 # get rid of NAs
   diatomTDI2$TDI3 <-  as.numeric(diatomTDI2$TDI3)
-  diatomTDI2$TDI3[diatomTDI2$TDI3 == "NA"] <-  0
   diatomTDI2$Abundance <- as.numeric(diatomTDI2$Abundance)
   diatomTDI2$score <- diatomTDI2$TDI4 * diatomTDI2$Abundance # create scores for TDI4 'as' value
   diatomTDI2$score3 <- diatomTDI2$TDI3 * diatomTDI2$Abundance # create scores for TDI3 'as' value
-  diatomTDI2$LTDI2[diatomTDI2$LTDI2 == "NA"] <- 0
-  diatomTDI2$LTDI1[diatomTDI2$LTDI1 == "NA"] <- 0
   diatomTDI2$scoreL4 <- diatomTDI2$LTDI2 * diatomTDI2$Abundance # create scores for LTDI2 'as' value
   diatomTDI2$scoreL3 <- diatomTDI2$LTDI1 * diatomTDI2$Abundance # create scores for LTDI2 'as' value + for NEMS TDi3 use $LTDLR for mastertaxonlist use $LTDI1
   diatomTDI2$Date <- as.Date(diatomTDI2$Date, "%d-%b-%Y")
@@ -36,42 +28,40 @@ darleqFunc <- function(diatomTDI2){
   diatomTDI2$LochRefValue2[diatomTDI2$Alk < 10] = 22 ## Create LTDI2 reference/expected diatom index value based on three Alkalinity bands
   diatomTDI2$LochRefValue2[diatomTDI2$Alk > 50] = 42
   
-
-  
  dataf <- lapply(split(diatomTDI2, diatomTDI2$SampleID), function(TDI){ #creates a new function which applies a series of commands on the split data by sample ID  
-  
+
    ## Abundances & Sums:
   
-  sumAbund <- sum(TDI$Abundance[TDI$Planktic != TRUE]) # sum abundance but exclude planktonic
-  tdi3SumAbund <- sum(TDI$Abundance[TDI$TDI3 > 0])
-  tdi4SumAbund <- sum(TDI$Abundance[TDI$TDI4 > 0])# sum abundance of scoring TDI4 taxa
+  sumAbund <- sum(TDI$Abundance[TDI$Planktic != TRUE],na.rm=TRUE) # sum abundance but exclude planktonic
+  tdi3SumAbund <- sum(TDI$Abundance[TDI$TDI3 > 0],na.rm=TRUE)
+  tdi4SumAbund <- sum(TDI$Abundance[TDI$TDI4 > 0],na.rm=TRUE)# sum abundance of scoring TDI4 taxa
   allTaxaAbund <- nrow(TDI) # count all taxa (the number of rows i.e. should be a row per taxa)
-  allTaxaAbund3 <- sum(TDI$Abundance[TDI$TDI3 >= 0]) # count all taxa abundance checking TDI.x field is not null/na 
-  allTaxaAbund4 <-  sum(TDI$Abundance[TDI$TDI4 >= 0]) # count all taxa abundance checking TDI.x field is not null/na 
-  notPlankticAbund <- sum(TDI$Abundance[TDI$Planktic == FALSE]) # count non-planktonic taxa to fit with NEMS field: 'Abundance of non-planktonic taxa'
+  allTaxaAbund3 <- sum(TDI$Abundance[TDI$TDI3 >= 0],na.rm=TRUE) # count all taxa abundance checking TDI.x field is not null/na 
+  allTaxaAbund4 <-  sum(TDI$Abundance[TDI$TDI4 >= 0],na.rm=TRUE) # count all taxa abundance checking TDI.x field is not null/na 
+  notPlankticAbund <- sum(TDI$Abundance[TDI$Planktic == FALSE],na.rm=TRUE) # count non-planktonic taxa to fit with NEMS field: 'Abundance of non-planktonic taxa'
   
   #### percentages TDI4:
   
-  plankticAbund <- sum(TDI$Abundance[TDI$Planktic == TRUE]) # count planktonic taxa
+  plankticAbund <- sum(TDI$Abundance[TDI$Planktic == TRUE],na.rm=TRUE) # count planktonic taxa
   plankticPercent <- (plankticAbund / allTaxaAbund4) * 100 # percentage planktonic using TDI4
-  motileAbund <- sum(TDI$Abundance[TDI$Motile == TRUE]) # sum abundance of motile taxa
+  motileAbund <- sum(TDI$Abundance[TDI$Motile == TRUE],na.rm=TRUE) # sum abundance of motile taxa
   motilePercent <- (motileAbund / allTaxaAbund4) * 100 # percentage motile using TDI4
-  organicAbund <- sum(TDI$Abundance[TDI$OrganicTolerant == TRUE])  # sum abundance of organic taxa
+  organicAbund <- sum(TDI$Abundance[TDI$OrganicTolerant == TRUE],na.rm=TRUE)  # sum abundance of organic taxa
   organicPercent <- (organicAbund / allTaxaAbund4) * 100 # percentage organic using TDI4
   
   #### percentages TDI3:
   
-  plankticAbund3 <- sum(TDI$Abundance[TDI$Planktic == TRUE]) # count planktonic taxa
+  plankticAbund3 <- sum(TDI$Abundance[TDI$Planktic == TRUE],na.rm=TRUE) # count planktonic taxa
   plankticPercent3 <- (plankticAbund3 / allTaxaAbund3) * 100 # percentage planktonic using TDI3
-  motileAbund3 <- sum(TDI$Abundance[TDI$Motile == TRUE]) # sum abundance of motile taxa
+  motileAbund3 <- sum(TDI$Abundance[TDI$Motile == TRUE],na.rm=TRUE) # sum abundance of motile taxa
   motilePercent3 <- (motileAbund3 / allTaxaAbund3) * 100 # percentage motile using TDI3
-  organicAbund3 <- sum(TDI$Abundance[TDI$OrganicTolerant == TRUE])  # sum abundance of organic taxa
+  organicAbund3 <- sum(TDI$Abundance[TDI$OrganicTolerant == TRUE],na.rm=TRUE)  # sum abundance of organic taxa
   organicPercent3 <- (organicAbund3 / allTaxaAbund3) * 100 # percentage organic using TDI3
   
   #### TDI3 & TDI4 & eTDI4 & eTDI3 scores:
   
-  as <- sum(TDI$score)
-  as3 <- sum(TDI$score3)
+  as <- sum(TDI$score,na.rm=TRUE)
+  as3 <- sum(TDI$score3,na.rm=TRUE)
   TDIscore <- ((as / tdi4SumAbund)*25) - 25 
   TDIscore3 <- ((as3 / tdi3SumAbund)*25) - 25
   eTDI <-  9.933*exp(log10(unique(TDI$Alk))*0.81)
@@ -85,17 +75,17 @@ darleqFunc <- function(diatomTDI2){
   
   ### Loch LTDI2 scores:
   
-  lochAbundsum <- sum(TDI$Abundance) # [TDI$LTDI2 > 0]) should this exclude zero scoring taxa? -
-  LTDI4SumAbund <- sum(TDI$scoreL4)
+  lochAbundsum <- sum(TDI$Abundance,na.rm=TRUE) # [TDI$LTDI2 > 0]) should this exclude zero scoring taxa? -
+  LTDI4SumAbund <- sum(TDI$scoreL4,na.rm=TRUE)
   w <- LTDI4SumAbund / lochAbundsum
   lochTDI4 <- (w * 25) - 25
   lochEQR4 <- (100 - (lochTDI4)) / (100 - (unique(TDI$LochRefValue2)))
   
   ### Loch LTDI1 scores:
   
-  totalabundsum <- sum(TDI$Abundance)
-  lochAbundsum2 <- sum(TDI$Abundance[TDI$LTDI1 > 0]) # should this exclude zero scoring taxa? -
-  LTDI3SumAbund <- sum(TDI$scoreL3) # issue with duplicate taxa in NEMS
+  totalabundsum <- sum(TDI$Abundance,na.rm=TRUE)
+  lochAbundsum2 <- sum(TDI$Abundance[TDI$LTDI1 > 0],na.rm=TRUE) # should this exclude zero scoring taxa? -
+  LTDI3SumAbund <- sum(TDI$scoreL3,na.rm=TRUE) # issue with duplicate taxa in NEMS
   w2 <- LTDI3SumAbund / lochAbundsum2
   lochTDI3 <- (w2 * 25) - 25
   lochEQR3 <- (100 - (lochTDI3)) / (100 - (unique(TDI$LochRefValue)))
